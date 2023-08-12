482ce887e36c26438d990de2496db2caf83d00d0
Rian <102740746+R4innn@users.noreply.github.com>
2023-08-12 21:23:49 +0700

    final commit

---
+++ app/build/generated/source/buildConfig/androidTest/debug/com/NotesUAS_10120202/test/BuildConfig.java
@@ -0,0 +1,10 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.NotesUAS_10120202.test;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.NotesUAS_10120202.test";
+  public static final String BUILD_TYPE = "debug";
+}
